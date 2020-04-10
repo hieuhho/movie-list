a3c473c7687d552b1d6804d7e01616546ed6de11
Hieu Ho <hihho@ucdavis.edu>
2020-04-10 00:04:40 -0700

    Added live data syntax

--- src/components/moviesData.js
+++ src/components/moviesData.js
@@ -1,27 +1,37 @@
 var moviesData = [
   {id: 1,
   title: 'Memories of Murder',
-  released: 'July 15, 2005',
+  release_date: 'July 15, 2005',
+  runtime: 1234,
+  vote_average: 132,
   director: 'Bong Joon-ho'},
 
   {id: 2,
   title: 'Perfect Blue',
-  released: 'August 5, 1997',
+  release_date: 'August 5, 1997',
+  runtime: 1234,
+  vote_average: 132,
   director: 'Satoshi Kon'},
 
   {id: 3,
   title: 'Oldboy (Korean)',
-  released: 'March 25, 2005',
+  release_date: 'March 25, 2005',
+  runtime: 1234,
+  vote_average: 132,
   director: 'Park Chan-wook'},
 
   {id: 4,
   title: 'Se7en',
-  released: 'September 22, 1995',
+  release_date: 'September 22, 1995',
+  runtime: 1234,
+  vote_average: 132,
   director: 'David Fincher'},
 
   {id: 5,
   title: 'Taxi Driver',
-  released: 'February 8, 1976',
+  release_date: 'February 8, 1976',
+  runtime: 1234,
+  vote_average: 132,
   director: 'Martin Scorsese'}
 ];
 
